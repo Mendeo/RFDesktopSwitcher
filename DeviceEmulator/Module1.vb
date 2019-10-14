292faf8d204872522c4cc05b685b711a9f4bdcd9
Themen <thesolve@mail.ru>
2019-10-14 14:21:54 +0300

    Добавил вывод сообщений о присутствии устройства в отдельный поток. Работаю с этими сообщениями в основной программе.

--- DeviceEmulator/Module1.vb
+++ DeviceEmulator/Module1.vb
@@ -2,25 +2,58 @@
 Option Strict On
 
 Module Module1
-    Private mDataReceived As Boolean = False
-    Sub Main()
-        Dim port As New IO.Ports.SerialPort("COM1", 115200, IO.Ports.Parity.None, 8, IO.Ports.StopBits.One)
-        port.ReceivedBytesThreshold = 1
-        AddHandler port.DataReceived, Sub() mDataReceived = True
-        port.Open()
+    Private Const READY As Char = "r"c
+    Private Const IN_WORK As Char = "w"c
+    Private Const ANSWER As Char = "o"c
+    Private Const FIRE As Char = "f"c
+    Private Const SCAN_TIMEOUT As Integer = 100
+    Private Const CHECK_DEVICE_TIMEOUT As Integer = 1000
 
+    Private mDataReceived As Boolean = False
+    Private mPort As New IO.Ports.SerialPort("COM1", 115200, IO.Ports.Parity.None, 8, IO.Ports.StopBits.One)
+    Private mMutex As New Threading.Mutex()
+
+    Sub Main()
+
+        mPort.ReceivedBytesThreshold = 1
+        AddHandler mPort.DataReceived, Sub() mDataReceived = True
+        mPort.Open()
+
+        Console.WriteLine("ready")
         Dim buff(0) As Char
         Do
             If mDataReceived Then
                 mDataReceived = False
-                port.Read(buff, 0, 1)
-                If buff(0) = "o"c Then
+                mPort.Read(buff, 0, 1)
+                If buff(0) = ANSWER Then
                     Exit Do
                 End If
             End If
-            port.Write("r"c)
-            Threading.Thread.Sleep(100)
+            mPort.Write(READY)
+            Threading.Thread.Sleep(SCAN_TIMEOUT)
         Loop
-        port.Write("w"c)
+        Console.WriteLine("working")
+        'Запускаем поток working
+        'Dim t As New Task(AddressOf working)
+        Dim tWorking As New Threading.Thread(New Threading.ThreadStart(AddressOf working))
+        tWorking.Start()
+        Console.WriteLine("Echo:")
+        Do
+            Dim str As String = Console.ReadLine
+            'Ожидаем завершения отправки сообщения в потоке tWorking
+            mMutex.WaitOne()
+            'Отсылаем то, что пришло из консоли.
+            mPort.Write(str)
+            'Снова запускаем отправку сообщений в потоке tWorking
+            mMutex.ReleaseMutex()
+        Loop
+    End Sub
+    Private Sub working()
+        Do
+            mMutex.WaitOne()
+            mPort.Write(IN_WORK)
+            mMutex.ReleaseMutex()
+            Threading.Thread.Sleep(CHECK_DEVICE_TIMEOUT)
+        Loop
     End Sub
 End Module
--- WinDSDriver/Form1.vb
+++ WinDSDriver/Form1.vb
@@ -1,26 +1,54 @@
 ﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
+    Private Const READY As Char = "r"c
+    Private Const IN_WORK As Char = "w"c
+    Private Const ANSWER As Char = "o"c
+    Private Const FIRE As Char = "f"c
+    Private Const SCAN_TIMEOUT As Integer = 100
+    Private Const CHECK_DEVICE_TIMEOUT As Integer = 1000
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim hasConnection As Boolean = False
+        Dim buff(0) As Char
+        devPort.ReadTimeout = SCAN_TIMEOUT
         For Each sp As String In My.Computer.Ports.SerialPortNames
-            console_RTB.AppendText(sp)
             devPort.PortName = sp
-            Dim buff(0) As Char
             Try
                 devPort.Open()
                 devPort.Read(buff, 0, 1)
             Catch ex As Exception
                 buff(0) = vbNullChar
             End Try
-            If buff(0) = "r"c Then
-                console_RTB.AppendText("+")
-            Else
-                console_RTB.AppendText("-")
+            If buff(0) = READY Then
+                hasConnection = True
+                Exit For
             End If
-            console_RTB.AppendText(vbCrLf)
             devPort.Close()
         Next
+        If Not hasConnection Then
+            MsgBox("Устройство не найдено.", MsgBoxStyle.Exclamation)
+            Return
+        End If
+        devPort.Write(ANSWER)
+        'Читаем порт, проверяем, что устройство подключено.
+        devPort.ReadTimeout = CHECK_DEVICE_TIMEOUT
+        Do
+            Try
+                devPort.Read(buff, 0, 1)
+            Catch ex As Exception
+                MsgBox("Устройство перестало отвечать.", MsgBoxStyle.Critical)
+                Return
+            End Try
+            If Not buff(0) = IN_WORK Then
+                If buff(0) = FIRE Then
+                    onFire()
+                Else
+                    MsgBox("От устройства пришли неожиданные данные.", MsgBoxStyle.Critical)
+                    Return
+                End If
+            End If
+        Loop
+    End Sub
+    Private Sub onFire()
+        console_RTB.AppendText("Fire!" & vbCrLf)
     End Sub
 End Class
