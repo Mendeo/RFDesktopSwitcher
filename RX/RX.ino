5c0b3a31206973a99dca7f0e348f378feab85b20
Themen <thesolve@mail.ru>
2019-10-19 21:18:20 +0300

    Сделал динамическое включение и отключение прерывания. После расшифровки сообщения о возникновении события, повторные сообщения не принимаются в течение восьми секунд. Это сделал, чтобы он в Serial слал только одну 'f' а не несколько, т.к. передатчик пять раз повторяет посылку.

--- RX/RX.ino
+++ RX/RX.ino
@@ -7,12 +7,15 @@
 #define COMMAND_DISCONNECT 'd'
 #define SCAN_TIMEOUT 100
 #define CHECK_DEVICE_TIMEOUT 1000
+#define PAUSE_TIME 8000
 
 const uint8_t RX_DATA[] = {12, 43, 29, 42, 24};
 unsigned long _sysTime;
 unsigned long _lastInWorkSend = 0;
 unsigned long _lastFireSend = 0;
+unsigned long _startPauseTime = 0;
 bool _isReadyStatus = true;
+bool _needWaitPause = false;
 
 volatile unsigned long _RXCurrentTime = 0;
 volatile unsigned long _RXPreviousTime = 0;
@@ -24,7 +27,6 @@ void setup()
 {
   pinMode(RX_PIN, INPUT);
   Serial.begin(115200);
-  attachInterrupt(digitalPinToInterrupt(RX_PIN), onPulse, CHANGE);
 }
 
 void onPulse()
@@ -60,6 +62,7 @@ void loop()
         {
           _isReadyStatus = false;
           Serial.flush();
+          attachInterrupt(digitalPinToInterrupt(RX_PIN), onPulse, CHANGE);
           break;
         }
       }
@@ -80,12 +83,21 @@ void loop()
     if (Serial.read() == COMMAND_DISCONNECT)
     {
       Serial.flush();
+      detachInterrupt(digitalPinToInterrupt(RX_PIN));
       _isReadyStatus = true;
     }
   }
   if (_isFire)
   {
     _isFire = false;
+    detachInterrupt(digitalPinToInterrupt(RX_PIN)); //Отключаем перывания на время паузы
+    _needWaitPause = true;
+    _startPauseTime = _sysTime;
     Serial.write(COMMAND_FIRE);
+  }
+  if (_needWaitPause && _sysTime - _startPauseTime >= PAUSE_TIME) //Ждём PAUSE_TIME милисекунд после события _isFire.
+  {
+    _needWaitPause = false;
+    attachInterrupt(digitalPinToInterrupt(RX_PIN), onPulse, CHANGE);
   }
 }
