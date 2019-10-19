713d072b8c30294c60b0993ec95d3f3806e4cd2a
Themen <thesolve@mail.ru>
2019-10-19 12:57:49 +0300

    Написал код для Ардуино.

--- RX/RX.ino
+++ RX/RX.ino
@@ -1,47 +1,55 @@
 #define RX_PIN 7
 #define ERROR_VALUE 2
-#define PAUSE_TIME 8000
-#define WAIT_SIMBOL 'r'
-#define IN_WORK_SIMBOL 'w'
-#define COMP_ANSWER 'o'
+#define COMMAND_READY 'r'
+#define COMMAND_IN_WORK 'w'
+#define COMMAND_ANSWER 'o'
+#define COMMAND_FIRE 'f'
+#define COMMAND_DISCONNECT 'd'
+#define SCAN_TIMEOUT 100
+#define CHECK_DEVICE_TIMEOUT 1000
 
 const uint8_t RX_DATA[] = {12, 43, 29, 42, 24};
 unsigned long _sysTime;
+unsigned long _lastInWorkSend = 0;
 uint8_t _rxCounter = 0;
+bool _isReadyStatus = true;
 
 
 void setup()
 {
   pinMode(RX_PIN, INPUT);
   Serial.begin(115200);
-  Serial.println("Start");
-  uint8_t buf;
-  while (true)
-  {
-    if (Serial.available())
-    {
-      buf = Serial.read();
-      if (buf == COMP_ANSWER)
-      {
-        Serial.write(IN_WORK_SIMBOL);
-        break;
-      }
-    }
-    Serial.write(WAIT_SIMBOL);
-    delay(100);
-  }
 }
 
 void loop()
 {
+  //Режим ожидания подключения.
+  if (_isReadyStatus)
+  {
+    while (true)
+    {
+      if (Serial.available())
+      {
+        if (Serial.read() == COMMAND_ANSWER)
+        {
+          _isReadyStatus = false;
+          Serial.flush();
+          break;
+        }
+      }
+      Serial.write(COMMAND_READY);
+      delay(SCAN_TIMEOUT);
+    }
+  }
   _sysTime = millis();
+  //Расшифровка данных от приёмника.
   if (_rxCounter % 2 == 0)
   {
-    while (digitalRead(RX_PIN)){}
+    waiter(digitalRead(RX_PIN));
   }
   else
   {
-    while (!digitalRead(RX_PIN)){}
+    waiter(!digitalRead(RX_PIN));
   }
   unsigned int len = millis() - _sysTime;
   if (len >= RX_DATA[_rxCounter] - ERROR_VALUE && len <= RX_DATA[_rxCounter] + ERROR_VALUE)
@@ -54,8 +62,32 @@ void loop()
   }
   if (_rxCounter == sizeof(RX_DATA))
   {
-    Serial.println("Ok!");
-    delay(PAUSE_TIME);
+    Serial.write(COMMAND_FIRE);
     _rxCounter = 0;
-  }  
+  }
+}
+
+ //Ожидаем пока работает заданное условие, при этом посылаем в консоль сообщения, что мы ещё на связи.
+void waiter(bool condition)
+{
+  unsigned long _curTime;
+  while (condition)
+  {
+    //Отправка символа работы.
+    _curTime = millis();
+    if (_curTime - _lastInWorkSend >= CHECK_DEVICE_TIMEOUT)
+    {
+      _lastInWorkSend = _curTime;
+      Serial.write(COMMAND_IN_WORK);
+    }
+    if (Serial.available())
+    {
+      if (Serial.read() == COMMAND_DISCONNECT)
+      {
+        Serial.flush();
+        _isReadyStatus = true;
+        break;
+      }
+    }
+  }
 }
